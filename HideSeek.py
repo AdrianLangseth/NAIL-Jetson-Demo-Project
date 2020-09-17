da14583d7a6c717602cfa39f4ee62a62a33099e2
Adrian <ad.langseth@gmail.com>
2020-09-17 16:03:28 +0200

    Swap Script file with a updated one.

--- HideSeek.py
+++ HideSeek.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Første forsøk på sammenmosing
+# # Hide 'n' Seek
 
-# In[1]:
 
 
 # This is a cell we must run from the start because the robot's nvargus daemon acts up and throws camera errors like nobody's business.
@@ -16,8 +15,6 @@ os.system('echo %s | %s' % (password, command))
 
 # ### Imports
 
-# In[2]:
-
 
 # As we are writing in a seperate folder we need to add paths to the other modules not contained in the current system paths.
 import sys
@@ -25,13 +22,11 @@ sys.path.insert(1,'/home/jetbot')
 sys.path.insert(1,'/home/jetbot/monodepth2')
 
 
-# In[3]:
+
+from __future__ import absolute_import, division, print_function  # unknown what this is but everything crashes if i remove it.
 
 
-from __future__ import absolute_import, division, print_function  # unknown what this is but everything fails if i remove it.
-get_ipython().run_line_magic('matplotlib', 'inline')
-
-# Utility functions for type conversions 
+# Utility functions for type conversions
 from jetson.utils import cudaFromNumpy
 import numpy as np
 
@@ -43,9 +38,10 @@ import matplotlib.pyplot as plt
 import torch
 import torchvision
 from torchvision import transforms
+import torch.nn.functional as Fun
 
 
-# Monodepth
+# Monocular Depth Perception
 from monodepth2 import *
 import monodepth2.networks as networks
 from monodepth2.utils import download_model_if_doesnt_exist
@@ -63,10 +59,7 @@ import PIL.Image as pil
 from jetson import inference
 
 
-
 # ## General Setups
-
-# In[4]:
 
 
 # If GPU is available, we will use it for efficiency
@@ -78,8 +71,6 @@ else:
 if __name__ == '__main__':
     print(device)
 
-
-# In[20]:
 
 
 # Instanciate camera with defined size.
@@ -94,7 +85,7 @@ except NameError:
 
 # Instanciate Camera
 robot = Robot()
-robot.stop()
+robot.stop() # Sometimes the robot start driving when instanciated, so this is in case it does this.
 
 def geometric_average(l:list):
     '''
@@ -109,8 +100,6 @@ blocking_threshold = 0.85
 
 # ## OF Setup
 
-# In[6]:
-
 
 # Instanciate our Detection Network from the Jetson Inference package with the model name and the threshold level.
 net = inference.detectNet("ssd-mobilenet-v2", threshold=0.4)
@@ -122,8 +111,6 @@ accepted_classes = [47]  # classID from coco_labels.txt file
 
 
 # ## Setting up CA model
-
-# In[7]:
 
 
 # In thsi block we create and customize our Collision avoidance model.
@@ -141,7 +128,7 @@ normalize = torchvision.transforms.Normalize(mean, stdev)
 def preprocess(camera_value): # Preprocessing for camera format --> Network input format
     """
     Preprocessing function for camera format --> network input format
-    
+
     param camera_value: input from Camera
     type camera_value: np.ndarray
     """
@@ -158,7 +145,6 @@ def preprocess(camera_value): # Preprocessing for camera format --> Network inpu
 
 # ## Setting up Monodepth model
 
-# In[8]:
 
 
 # We build our monocular depth estimation model from the Monodepth module
@@ -184,50 +170,43 @@ depth_decoder.load_state_dict(loaded_dict)
 encoder.eval()
 depth_decoder.eval();
 
-#pull out feed size to use for processing in loop, do now for efficiency in execution 
+#pull out feed size to use for processing in loop, do now for efficiency in execution
 feed_height = loaded_dict_enc['height']
 feed_width = loaded_dict_enc['width']
 
 
 # ## CA Prep
 
-# In[9]:
-
-
-import torch.nn.functional as Fun
-
 
 def CA_update(change):
     """
     This function receives the change in the camera values and acts on it if the confidence of a blocked path is above the blocking threshold.
     This function forms the basis of the collision avoidance safety for the monocular depth perception.
-    
+
     param change: The change observed in the camera
     type change: Dictionary
     """
     global robot, prob_blocked
-    x = change['new'] 
+    x = change['new']
     x = preprocess(x)
     y = model(x)
-    
+
     # we apply the `softmax` function to normalize the output vector so it sums to 1 (which makes it a probability distribution)
     y = Fun.softmax(y, dim=1)
-    
+
     # If the blocking confidence is higher than the set threshold the smart collision avoidance overrides the motor functions of the monocular depth estimator.
     prob_blocked = float(y.flatten()[0])
-    if prob_blocked > blocking_threshold: 
-        # The CA aspect chooses the turn based on its previous motor values. The basic concept being a smooth turn is the one continuing on the already progressing one. 
+    if prob_blocked > blocking_threshold:
+        # The CA aspect chooses the turn based on its previous motor values. The basic concept being a smooth turn is the one continuing on the already progressing one.
         if robot.left_motor.value > robot.right_motor.value:
             robot.right(0.4)
         else:
             robot.left(0.4)
-        
+
     time.sleep(0.01)
 
 
 # ## OD Prep
-
-# In[10]:
 
 
 def OD_update(change):
@@ -235,13 +214,13 @@ def OD_update(change):
     This function receives the change in the camera values and runs object detection. If a detection of the desired class is made,
     OD overrides the normal functionality to track down the detected object.
     This function forms the basis of the endgame override to the monocular depth perception.
-    
+
     param change: The change observed in the camera
     type change: Dictionary
     """
     global robot, cam, det
     try: # Preprocessing of input values
-        arr = change['new'] 
+        arr = change['new']
         frame = cv2.cvtColor(arr, cv2.COLOR_BGR2RGBA) # input is on RBGR, network needs RGBA.
         camCuda = cudaFromNumpy(frame)
         detections = net.Detect(image = camCuda, width = camwidth, height = camheight, overlay = "none")
@@ -258,7 +237,7 @@ def OD_update(change):
                 cam.unobserve_all()
                 print("Hider Located! Victory!")
             else:
-                x = center_x/camwidth - 0.5 
+                x = center_x/camwidth - 0.5
                 robot.set_motors(
                     float(0.4 + 0.8 * x), # left
                     float(0.4 - 0.8 * x)) # Right
@@ -268,20 +247,18 @@ def OD_update(change):
 
 # # Run
 
-# In[24]:
-
 
 CA_update({'new': cam.value}) # We instanciate the update functions with the current camera values
 OD_update({'new': cam.value})
-cam.observe(CA_update, names='value') # Now we connect the update functions to the camera. This wil cause the update functions o be called whenever we have a change in the camera values.
+cam.observe(CA_update, names='value') # Now we connect the update functions to the camera. This will cause the update functions to be called whenever we have a change in the camera values.
 cam.observe(OD_update, names='value')
-while True: # Run indefinitely, only ended when we have success or we have an resignation by KeyboardInterrupt.
+while True: # Run indefinitely, only ended when we have success or we have a resignation by KeyboardInterrupt.
     try:
         if det:
             cam.unobserve(CA_update, names='value')
             break
         elif prob_blocked < blocking_threshold:
-            # preprocessing: 
+            # preprocessing:
             inputImage = pil.fromarray(cam.value.astype('uint8'),'RGB') #.rotate(180)
             input_image_resized = inputImage.resize((feed_width, feed_height), pil.LANCZOS)
             input_image_pytorch = transforms.ToTensor()(input_image_resized).unsqueeze(0).cuda()
@@ -291,11 +268,11 @@ while True: # Run indefinitely, only ended when we have success or we have an re
                 features = encoder(input_image_pytorch)
                 outputs = depth_decoder(features)
             disp = outputs[("disp", 0)]
-            
+
             # postprocessing
             disp_resized = torch.nn.functional.interpolate(disp.cpu(), (camheight, camwidth), mode="bilinear", align_corners=False).cuda()
             disp_resized_np = disp_resized.squeeze().cpu().numpy()
-            
+
             # Algorithm for evaluating depth perceptions and pushing onto motor values
             A = []
             B = []
@@ -312,14 +289,13 @@ while True: # Run indefinitely, only ended when we have success or we have an re
             B_scale = 1 - geometric_average(B)/np.max(disp_resized_np)
             right_motor = 2*(0.5 - geometric_average(A)*B_scale)
             left_motor = 2*(0.5 - geometric_average(C)*B_scale)
-            robot.set_motors(left_motor, right_motor)           
+            robot.set_motors(left_motor, right_motor)
     except KeyboardInterrupt: # Set up a keyboard interrupt so that if we abort from keyboard it will dismantle properly and not SIGKILL.
         cam.unobserve_all()
         robot.stop()
-        print('Loop successfully ended, loop')
+        print('Loop successfully ended')
         break
-        
+
 # We end up here if we successfully find our object, therefore we cant dismantle all camera observables and for safety also stop robot.
 cam.unobserve_all()
 robot.stop()
-
